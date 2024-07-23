67e7cecc18c7962ae4729d4c0a1377d5575b7c28
Dmitry Ryazantsev <ryazantsev_03@bk.ru>
2024-07-23 21:15:21 +0500

    init commit

---
+++ src/test/java/com/app/filestore/FileStoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.filestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileStoreApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
