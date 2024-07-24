9087f0eea5f1e5bdbc73586685fa5eb69e529d6f
Dmitry Ryazantsev <ryazantsev_03@bk.ru>
2024-07-24 09:36:43 +0500

    feat: Added validation for request

--- src/main/java/com/app/filestore/dto/FileDtoRequest.java
+++ src/main/java/com/app/filestore/dto/FileDtoRequest.java
@@ -1,13 +1,18 @@
 package com.app.filestore.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
 
 import java.time.LocalDate;
 
 public record FileDtoRequest(
+        @NotBlank(message = "file must not be blank")
         String file,
+        @NotBlank(message = "title must not be blank")
         String title,
-        @JsonFormat(pattern = "yyyy-MM-dd")
+        @NotNull(message = "creationTime must not be null")
+        @PastOrPresent(message = "creationTime must be in the past or in the present")
         LocalDate creationTime
 ) {
 }
