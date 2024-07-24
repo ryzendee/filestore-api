d4a1567857765ea82eeec0d500c1e76b377d81d5
Dmitry Ryazantsev <ryazantsev_03@bk.ru>
2024-07-24 08:39:45 +0500

    refactor: Added response status

--- src/main/java/com/app/filestore/controller/FileController.java
+++ src/main/java/com/app/filestore/controller/FileController.java
@@ -5,9 +5,11 @@ import com.app.filestore.dto.FileDtoResponse;
 import com.app.filestore.service.FileService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @RestController
 @RequestMapping("/api/v1/files")
@@ -16,7 +18,9 @@ public class FileController {
 
     private final FileService fileService;
 
+
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public FileDtoResponse saveFile(@Valid FileDtoRequest fileDtoRequest) {
         return fileService.saveFile(fileDtoRequest);
     }
