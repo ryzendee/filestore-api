819d23380f52272a29e6d04a758f3a3dd39810bb
Dmitry Ryazantsev <ryazantsev_03@bk.ru>
2024-07-24 10:03:30 +0500

    feat: Implemented getFileById method in service

--- src/main/java/com/app/filestore/service/FileService.java
+++ src/main/java/com/app/filestore/service/FileService.java
@@ -1,9 +1,11 @@
 package com.app.filestore.service;
 
+import com.app.filestore.dto.FileDto;
 import com.app.filestore.dto.FileDtoRequest;
 import com.app.filestore.dto.FileDtoResponse;
 
 public interface FileService {
 
     FileDtoResponse saveFile(FileDtoRequest fileDtoRequest);
+    FileDto getFileById(Long id);
 }
--- src/main/java/com/app/filestore/service/FileServiceImpl.java
+++ src/main/java/com/app/filestore/service/FileServiceImpl.java
@@ -1,12 +1,15 @@
 package com.app.filestore.service;
 
+import com.app.filestore.dto.FileDto;
 import com.app.filestore.dto.FileDtoRequest;
 import com.app.filestore.dto.FileDtoResponse;
 import com.app.filestore.entity.FileEntity;
+import com.app.filestore.exception.file.FileNotFoundException;
 import com.app.filestore.mapper.FileMapper;
 import com.app.filestore.repository.FileRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 
 @Service
@@ -16,11 +19,20 @@ public class FileServiceImpl implements FileService {
     private final FileMapper fileMapper;
     private final FileRepository fileRepository;
 
+    @Transactional
     @Override
     public FileDtoResponse saveFile(FileDtoRequest fileDtoRequest) {
         FileEntity entity = fileMapper.map(fileDtoRequest);
         fileRepository.save(entity);
 
         return new FileDtoResponse(entity.getId());
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public FileDto getFileById(Long id) {
+        return fileRepository.findById(id)
+                .map(fileMapper::map)
+                .orElseThrow(() -> new FileNotFoundException("Failed to find file with id: " + id));
     }
 }
