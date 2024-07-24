6e301cedd21aa27121893b3fec4fde7213b11fa7
Dmitry Ryazantsev <ryazantsev_03@bk.ru>
2024-07-24 13:43:37 +0500

    feat: Added HttpMessageNotReadableException handling

--- src/main/java/com/app/filestore/controller/ControllerExceptionHandler.java
+++ src/main/java/com/app/filestore/controller/ControllerExceptionHandler.java
@@ -4,6 +4,7 @@ import com.app.filestore.dto.response.ErrorDtoResponse;
 import com.app.filestore.dto.response.ValidationErrorDtoResponse;
 import com.app.filestore.exception.file.FileNotFoundException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -16,6 +17,12 @@ import java.util.List;
 @RestControllerAdvice
 public class ControllerExceptionHandler {
 
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorDtoResponse handleHttpMessageNotReadableException() {
+        return new ErrorDtoResponse("The request could not be processed due to malformed JSON or invalid data format. " +
+                "Please check the request body and try again");
+    }
     @ExceptionHandler(FileNotFoundException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorDtoResponse handleFileNotFoundException(FileNotFoundException ex) {
@@ -34,6 +41,7 @@ public class ControllerExceptionHandler {
         List<String> messages = getMessagesFromFieldErrors(bindingResult.getFieldErrors());
         return new ValidationErrorDtoResponse(messages);
     }
+
     private List<String> getMessagesFromFieldErrors(List<FieldError> fieldErrors) {
         return fieldErrors.stream()
                 .map(fieldError -> fieldError.getField() + " : " + fieldError.getDefaultMessage())
