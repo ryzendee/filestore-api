815e33de093375117c0a4d8821cb3add68df1842
Dmitry Ryazantsev <ryazantsev_03@bk.ru>
2024-07-24 10:03:51 +0500

    feat: Added FileNotFoundException

---
+++ src/main/java/com/app/filestore/exception/file/FileNotFoundException.java
@@ -0,0 +1,8 @@
+package com.app.filestore.exception.file;
+
+public class FileNotFoundException extends RuntimeException {
+
+    public FileNotFoundException(String message) {
+        super(message);
+    }
+}
