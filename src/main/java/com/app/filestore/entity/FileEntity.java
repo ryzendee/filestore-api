3a27b7d529d46f3879385b0ee4d3948b396e1430
Dmitry Ryazantsev <ryazantsev_03@bk.ru>
2024-07-25 08:32:03 +0500

    feat: Added toString for entity and logging in service

--- src/main/java/com/app/filestore/entity/FileEntity.java
+++ src/main/java/com/app/filestore/entity/FileEntity.java
@@ -1,10 +1,7 @@
 package com.app.filestore.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDate;
 
@@ -13,6 +10,7 @@ import java.time.LocalDate;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 @Table(name = "files")
 public class FileEntity {
 
--- src/main/java/com/app/filestore/service/FileServiceImpl.java
+++ src/main/java/com/app/filestore/service/FileServiceImpl.java
@@ -8,6 +8,7 @@ import com.app.filestore.exception.file.FileNotFoundException;
 import com.app.filestore.mapper.FileMapper;
 import com.app.filestore.repository.FileRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -17,6 +18,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class FileServiceImpl implements FileService {
 
@@ -28,7 +30,7 @@ public class FileServiceImpl implements FileService {
     public FileCreateDtoResponse saveFile(FileCreateDtoRequest fileCreateDtoRequest) {
         FileEntity entity = fileMapper.map(fileCreateDtoRequest);
         fileRepository.save(entity);
-
+        log.info("Saved file: {}", entity);
         return new FileCreateDtoResponse(entity.getId());
     }
 
